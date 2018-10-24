e07d255dd1433680aba62a0931b09ea7b758b9c9
landa95 <95landa@gmail.com>
2018-10-24 17:53:06 +0200

    Favourites Fragment and layout init

--- app/src/main/java/eus/ilanda/eskatuetaordaindu/fragments/FragmentFavourites.java
+++ app/src/main/java/eus/ilanda/eskatuetaordaindu/fragments/FragmentFavourites.java
@@ -1,6 +1,20 @@
 package eus.ilanda.eskatuetaordaindu.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import eus.ilanda.eskatuetaordaindu.R;
 
 public class FragmentFavourites extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_favourites, null);
+        return v;
+    }
 }
